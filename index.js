7e7697d9ba008a1a1cbeaeadb52d2c60de5c26dd
shambashib20 <shambashibmajumdar2000@gmail.com>
2021-06-03 21:25:02 +0530

    commits

--- index.js
+++ index.js
@@ -3,4 +3,37 @@ const server = require("http").createServer(app);
 const cors = require("cors");
 
 // Calling the required dependencies (1-3)
-// Testing the tracking
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send('Server is running.');
+});
+
+io.on('connection', (socket) => {
+    socket.emit('me', socket.id);
+    
+    socket.on('disconnect', () => {
+        socket.broadcast.emit("callended");
+    });
+
+    socket.on("calluser", ({ userToCall, signaldata, from, name }) => {
+        io.to(userToCall).emit("calluser",{ signal : signalData, from, name });
+    });
+
+    socket.on("answercall", (data) => {
+        io.to(data.to).emit("callaccepted", data.signal);
+    });
+
+});
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
